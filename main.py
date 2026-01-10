eda32dc8daba4005adb5f6c7146a869cd6095d34
Hsinghsudwal <sudwalh@gmail.com>
2026-01-10 10:55:51 -0800

    cli entrypoint

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
         epilog="""
 Examples:
   python main.py --goal baseline --dataset wine --dataset-path ./data/wine.csv
-  python main.py --goal ml_train --dataset wine --dataset-path ./data/wine.csv --prefix init --extra deploy --env dev
+  python main.py --goal ml_train --dataset wine --dataset-path ./data/wine.csv --prefix init --extra deploy --env local
   python main.py --goal deploy --dataset wine --dataset-path ./data/wine.csv --prefix prod
         """
     )
@@ -35,21 +35,7 @@ Examples:
         default="init",
         help="Artifact prefix (init, replay, hotfix, shadow, prod)"
     )
-    parser.add_argument("--extra", default="{}", help="Extra parameters suggestion deploy or k8s")
-    parser.add_argument(
-        "--env",
-        default="dev",
-        choices=["dev", "staging", "prod"],
-        help="Environment for deployment"
-    )
     
-    
-    # parser.add_argument("--fastapi", action="store_true")
-    # parser.add_argument("--cloud", action="store_true")
-    # parser.add_argument("--k8s", action="store_true")
-
-
-    # env_flags = {"fastapi": args.fastapi, "cloud": args.cloud, "k8s": args.k8s}
 
     args = parser.parse_args()
     
@@ -57,9 +43,7 @@ Examples:
         intent=args.goal,
         dataset=args.dataset,
         dataset_path=args.dataset_path,
-        prefix=args.prefix,
-        extra=args.extra,
-        env=args.env
+        prefix=args.prefix
     )
     
     result = orchestrator.run()
@@ -70,6 +54,6 @@ Examples:
         print(f"Execution failed: {result['error']}")
         exit(1)
 
+
 if __name__ == "__main__":
     main()
-    
